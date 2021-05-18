79dd1eacc91d695815529a2f760493ded9c00741
Doreen162 <doreenligman17@gmail.com>
2021-05-18 11:54:55 +0200

    Vuejs State Management

---
+++ src/components/TodosList.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   <todo v-for="(todo, index) in todos" :key="index" :todo="todo" />
+ </div>
+</template>
+
+<script>
+import Todo from "./Todo"
+
+export default {
+  components: {
+   Todo
+ },
+  computed: {
+    todos() {
+      return this.$store.state.todos;
+    }
+  }
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
